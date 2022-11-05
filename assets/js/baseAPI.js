17aa8969ee220362e1b70080545888e6a460eef7
zhouxun <zhouxun_1205@163.com>
2022-11-05 19:55:29 +0800

    完成了登录和注册功能

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,25 @@
+// 注意：每次调用 $.get()/$.post()/$.ajax() 的时候
+// 会先调用 ajaxPrefilter 这个函数
+// 在这个函数中，可以拿到我们给Ajax提供的配置对象
+$.ajaxPrefilter(function(options){
+    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+    options.url = 'http://www.liulongbin.top:3007' + options.url
+
+    // 统一位有权限的接口，设置 headers 请求头
+    if(options.url.indexOf('/my') !== -1){
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载 complete 回调函数
+    options.complete = function(res){
+        if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！'){
+            // 1 强制清空 token
+            localStorage.removeItem('token')
+            // 2 强制跳转到登录界面
+            location.href = './login.html'
+        }
+    }
+
+})
---
+++ assets/js/login.js
@@ -0,0 +1,68 @@
+$(function () {
+
+    // 点击去注册的链接
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+
+    // 点击去登录的链接
+    $('#link_login').on('click', function () {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+
+    // 从 layui 中获取 form 对象
+    var form = layui.form
+    form.verify({
+        pwd: [/^[\S]{6,12}$/,'密码必须6-12位，且不能包含空格'],
+        repwd: function(repwd){
+            var pwd = $('.reg-box [name="password"]').val()
+            if(repwd !== pwd){
+                return '两次密码输入不一致！'
+            }
+        }
+    })
+
+    var layer = layui.layer
+
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit', function(e){
+        // 1 阻止默认的提交行为
+        e.preventDefault()
+        // 2 发起ajax的post请求
+        data = {
+            username: $('.reg-box [name="username"]').val(),
+            password:$('.reg-box [name="password"]').val()
+        }
+        $.post('/api/reguser', data, function(res){
+            if(res.status !== 0){
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功，请登录！')
+            $('#link_login').click()
+        })
+    })
+
+    // 监听登录表单的提交事件
+    $('#form_login').submit(function(e){
+        // 阻止默认提交行为
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            // 快速获取表单中的数据
+            data: $(this).serialize(),
+            success: function(res){
+                if(res.status !== 0){
+                    return layer.msg('登录失败！' + res.message)
+                }
+                layer.msg('登录成功！')
+                localStorage.setItem('token', res.token)
+                location.href = './index.html'
+            }
+        })
+    })
+
+})
+
